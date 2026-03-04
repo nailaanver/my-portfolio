ecbad23392182a2a92a1a2fb1dd70f9f2dcf42b9
Fathima Naila <fathimanaila71@gmail.com>
2026-03-04 12:16:58 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import "../styles/Navbar.css";
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2 className="logo">Naila.dev</h2>
+
+      <ul className="nav-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
